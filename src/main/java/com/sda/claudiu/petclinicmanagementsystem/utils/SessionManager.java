cc6175102f4d3eaf761237e43f355a4b9a268be4
Claudiu Tat <clawtat@gmail.com>
2022-11-20 09:40:10 +0200

    PCM_S-14: Add Hibernate configuration (pom and utils).

---
+++ src/main/java/com/sda/claudiu/petclinicmanagementsystem/utils/SessionManager.java
@@ -0,0 +1,30 @@
+package com.sda.claudiu.petclinicmanagementsystem.utils;
+
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionManager extends AbstractSessionManager {
+
+    private static final SessionManager INSTANCE = new SessionManager();
+
+    private SessionManager() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return INSTANCE.getSessionFactory("book_management_system");
+    }
+
+    public static void shutDown() {
+        INSTANCE.shutdownSessionManager();
+    }
+
+    @Override
+    protected void setAnnotatedClasses(Configuration configuration) {
+        // Hibernate model will be added here
+
+    }
+
+
+}
