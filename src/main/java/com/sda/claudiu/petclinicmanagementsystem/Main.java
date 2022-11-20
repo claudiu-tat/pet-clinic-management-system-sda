8686a50198f9af3038705f2e3a1aae78929f98a2
Claudiu Tat <clawtat@gmail.com>
2022-11-20 16:42:11 +0200

    PCM_S-17: Create user menu.

--- src/main/java/com/sda/claudiu/petclinicmanagementsystem/Main.java
+++ src/main/java/com/sda/claudiu/petclinicmanagementsystem/Main.java
@@ -1,11 +1,43 @@
 package com.sda.claudiu.petclinicmanagementsystem;
 
+import com.sda.claudiu.petclinicmanagementsystem.menu.UserOption;
 import com.sda.claudiu.petclinicmanagementsystem.utils.SessionManager;
+
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
         SessionManager.getSessionFactory();
+
+        Scanner scanner = new Scanner(System.in);
+
+        UserOption userOption = UserOption.UNKNOWN;
+        do {
+            UserOption.printAllOptions();   // we print the options on the screen
+            System.out.println("Please insert an option:");
+            try {
+                int numberGiven = Integer.parseInt(scanner.nextLine());
+                userOption = UserOption.findUserOption(numberGiven);
+            } catch (NumberFormatException n) {
+                n.printStackTrace();
+                userOption = UserOption.UNKNOWN;
+            }
+
+            switch (userOption) {
+                case CREATE_VETERINARIAN:
+                    System.out.println("Not implemented!");
+                    break;
+                case EXIT:
+                    System.out.println("Good bye!");
+                    break;
+                case UNKNOWN:
+                    System.out.println("An unknown option!");
+                default:
+                    System.out.println("User option " + userOption + " not implemented!");
+                    break;
+            }
+        } while (userOption != UserOption.EXIT);
 
        // SessionManager.shutDown();
     }
---
+++ src/main/java/com/sda/claudiu/petclinicmanagementsystem/menu/UserOption.java
@@ -0,0 +1,42 @@
+package com.sda.claudiu.petclinicmanagementsystem.menu;
+
+import javax.print.DocFlavor;
+
+public enum UserOption {
+    CREATE_VETERINARIAN(1, "Create a veterinarian"),
+    EXIT(99, "Exit"),
+    UNKNOWN(100, "Unknown option");
+
+    private int numericOption;      // to have the possibility to pick a number, we have to declare the numeric option here
+    private String displayValue;    // here is the option displayed for specific number
+
+    UserOption(int numericOption, String displayValue) {    // with the constructor we can give the numeric option and the value in our menu
+        this.numericOption = numericOption;
+        this.displayValue = displayValue;
+    }
+
+    public int getNumericOption() {         // we need just getters because we don't want to change the value with setters, we just want to display the menu
+        return numericOption;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
+
+    public static void printAllOptions() {      // we need this method to display all options avl
+        for (UserOption value : UserOption.values()) {
+            if (value != UNKNOWN) {             // validate not to be unknown
+                System.out.println(value.getNumericOption() + " - " + value.getDisplayValue());     // display the option
+            }
+        }
+    }
+
+    public static UserOption findUserOption(int numericValue) {    // numericValue is the parameter we just give
+        for (UserOption value : UserOption.values()) {
+            if (value.numericOption == numericValue) {      // validate is numericValue is wright with the numericOption from menu
+                return value;
+            }
+        }
+        return UNKNOWN;
+    }
+}
